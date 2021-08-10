a1d87300bee28365ba0f18bc9c5f87ce92cd852c
lijandrew <li.jj.andrew@gmail.com>
2021-08-10 17:30:35 -0400

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "assets/[hash][ext]",
+        },
       },
     ],
   },
