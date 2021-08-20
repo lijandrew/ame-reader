5c87916e07705b1ce59ddde023545e470d909bdc
lijandrew <li.jj.andrew@gmail.com>
2021-08-19 20:40:00 -0400

    .

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ import "./index.scss";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
+/*
+if ("serviceworker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
+*/
+
 module.hot.accept();
